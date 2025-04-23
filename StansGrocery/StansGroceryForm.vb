5f8a2470b4026b1775c0b91acc4119af2e8430e6
Jason Permann <JaceFoxman@gmail.com>
2025-04-23 08:40:33 -0600

    Change Form name and add header

--- StansGrocery/StansGroceryForm.vb
+++ StansGrocery/StansGroceryForm.vb
@@ -2,7 +2,7 @@
 'Spring 2025
 'RCET2265
 'Stan's Grocery
-'
+'https://github.com/JaceFoxman/StansGrocery.git
 Public Class StansGroceryForm
     Private Sub StansGroceryForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
